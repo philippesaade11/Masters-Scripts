3d352d4912fc4f05241737158acf5bf9f92aad51
Philippe Saade <philippe.saade@lau.edu>
2023-10-14 14:52:14 +0200

    update add edges to include descriptions

--- neo4j_add_edges.py
+++ neo4j_add_edges.py
@@ -13,24 +13,29 @@ def add_node(session, node):
     result = session.run("CREATE (a:Wiki $node)", node=node)
         
 def add_edge(session, docid1, docid2, edge):
-    edge_name = edge['predicate_name'].replace(' ', '_')
-    result = session.run(f"MATCH (a:Wiki {{docid: $docid1}}) MATCH (b:Wiki {{docid: $docid2}}) MERGE (a)-[r:{edge_name}]->(b) SET r += $edge", docid1=docid1, docid2=docid2, edge=edge)
+    result = session.run("MATCH (a:Wiki {docid: $docid1}) MATCH (b:Wiki {docid: $docid2}) MERGE (a)-[r:Link]->(b) SET r += $edge", docid1=docid1, docid2=docid2, edge=edge)
 
 predicate_names = {}
-def get_predicate_name(uri):
+def get_predicate_name(triple):
+    uri = triple['predicate']['uri']
     prop_id = uri.split('/')[-1]
     if prop_id in predicate_names:
-        return predicate_names[prop_id]
+        return predicate_names[prop_id]['label'], predicate_names[prop_id]['description']
     
     url = f"https://www.wikidata.org/w/api.php?action=wbgetentities&ids={prop_id}&format=json"
     response = requests.get(url)
-    label = response.json()['entities'][prop_id]['labels']['en']['value']
-    predicate_names[prop_id] = label
-    return label
+    try:
+        label = response.json()['entities'][prop_id]['labels']['en']['value']
+        description = response.json()['entities'][prop_id]['descriptions']['en']['value']
+    except:
+        label = triple['predicate']['surfaceform']
+        description = ""
+        
+    predicate_names[prop_id] = {"label": label, "description": description}
+    return label, description
     
 #Expected number: 20877472
 data_dir = "/app/T-Rex"
-count = 0
 for file in os.listdir(data_dir):
     if '.json' in file:
         s_time = time.time()
@@ -42,20 +47,20 @@ for file in os.listdir(data_dir):
                 for t in d['triples']:
                     count += 1
                     try:
-                        predicate_name = get_predicate_name(t['predicate']['uri'])
+                        predicate_name, predicate_description = get_predicate_name(t)
                         entry = {
                             'predicate_surfaceform': t['predicate']['surfaceform'],
                             'predicate_uri': t['predicate']['uri'],
                             'predicate_name': predicate_name,
+                            'predicate_description': predicate_description,
                             'object_surfaceform': t['object']['surfaceform'],
                             'subject_surfaceform': t['subject']['surfaceform'],  
                         }
                         add_edge(session, t['subject']['uri'], t['object']['uri'], entry)
-                        
-                        with open("edge_update.csv", "a+") as f:
-                            f.write(f"{count},")
                     except Exception as e:
                         print(e)
+                        print(t)
+                        print()
 
         print(f"Time for file {file} is {int(time.time() - s_time)}sec")
         del data
